15bbaffe4e76fb6520379e96e19b425afcc23014
fchristian1 <fchristian1@gmx.net>
2025-02-20 21:27:20 +0100

    20250220-212720

--- 13_terraform_ansible_nginx/create.sh
+++ 13_terraform_ansible_nginx/create.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-terraform init -upgrade && terraform apply -auto-approve && ./ansible-Ping.sh && ./ansiblePlay.sh
+terraform init -upgrade && terraform apply -auto-approve && sleep 5 && ./ansible-Ping.sh && ./ansiblePlay.sh
